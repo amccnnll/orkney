6a493de92ff20d67d17e23f6b48be5e75817d422
amccnnll <alistair.mcconnell@gmail.com>
2017-05-30 13:18:04 +0100

    minor updates

--- working_pt1.R
+++ working_pt1.R
@@ -1,16 +1,20 @@
-reoriented.orkney <- t(orkney)
-orkney.df <- data.frame(reoriented.orkney)
+## orkney seals main plot
+## am
+## 2017-05-30
+
+# converting the orkney matrix to a 1D dataframe
+orkney.df <- data.frame(t(orkney))
 colnames(orkney.df) <- as.numeric(rev(Longitude))
 orkney.df <- cbind(Latitude, orkney.df)
-
 orkney.v <- gather(orkney.df, Longitude, Value, -Latitude)
 orkney.v$Longitude <- as.numeric(rev(orkney.v$Longitude))
 
-mstr <- ggplot(orkney.v, aes(x = Longitude, y = Latitude, fill = Value)) +
-  geom_tile() +
-  theme_bw() +
-  coord_cartesian() +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(expand = c(0,0))
+# main plot
+seals.main <- ggplot(orkney.v, aes(x = Longitude, y = Latitude, fill = Value)) +
+              geom_raster() +
+              theme_bw() +
+              coord_cartesian() +
+              scale_x_continuous(expand = c(0,0)) +
+              scale_y_continuous(expand = c(0,0))
 
-mstr
+seals.main
