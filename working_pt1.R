53f4b20061add41540ea9cb302d8b7a08f1209a4
amccnnll <alistair.mcconnell@gmail.com>
2017-05-30 16:24:49 +0100

    it's working! it's working!

--- working_pt1.R
+++ working_pt1.R
@@ -9,11 +9,20 @@ orkney.df <- cbind(Latitude, orkney.df)
 orkney.v <- gather(orkney.df, Longitude, Value, -Latitude)
 orkney.v$Longitude <- as.numeric(rev(orkney.v$Longitude))
 
+# converting seals output to dataframe
+sealsb <- as.data.frame(seals)
+sealsb$lon <- x2lon(sealsb$x)
+sealsb$lat <- y2lat(sealsb$y)
+
+
 # main plot
-seals.main <- ggplot(orkney.v, aes(x = Longitude, y = Latitude, fill = Value)) +
-              geom_raster() +
-              theme_bw() +
-              coord_cartesian() +
+seals.main <- ggplot(orkney.v, aes(x = Longitude,
+                                   y = Latitude)) +
+              geom_tile(data = orkney.v, aes(fill = as.factor(Value))) +
+              scale_fill_manual(values = c("white", "red", "green", "grey")) +
+              geom_path(data = sealsb, aes(x = lon, y = lat)) +
+              guides(fill = F) +
+              coord_equal() +
               scale_x_continuous(expand = c(0,0)) +
               scale_y_continuous(expand = c(0,0))
 
