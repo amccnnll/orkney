ef3dc97abd87687e2feb547a8853536d7cc43922
amccnnll <alistair.mcconnell@gmail.com>
2017-06-02 02:31:56 +0100

    pdf

--- foodmap_wrangling.R
+++ foodmap_wrangling.R
@@ -7,7 +7,7 @@ require(plotly)
 
 # converting the foodmap matrix to a data frame, only using the first foodmap.
 # transposing and flipping the x-axis to reorient north to the top
-food <- data.frame(t(foodmap[[1]]))
+food <- data.frame(t(foodmap[[4]]))
 colnames(food) <- as.numeric(rev(Longitude))
 food <- cbind(Latitude, food)
 food <- gather(food, Longitude, Value, -Latitude)
@@ -15,11 +15,13 @@ food$Longitude <- as.numeric(rev(food$Longitude))
 
 # quiver plot
 p <-  ggplot(data = food, aes(x = Longitude, y = Latitude)) +
-  geom_point(size = 1, colour = "red") +
+  geom_point(size = 0.5, colour = "red") +
   geom_spoke(aes(angle = Value), radius = 0.01) +
   coord_equal() +
   theme_bw() +
   scale_x_continuous(expand = c(0,0)) +
   scale_y_continuous(expand = c(0,0))
+
+ggsave("foodmap1.pdf", width = 594, height = 420, units = "mm")
 
 ggplotly(p)
